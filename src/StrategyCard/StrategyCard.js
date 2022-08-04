5f4fb3f6a9098108a3ceaf437ff25afe86161f1f
David Daw <daviddaw@gmail.com>
2022-08-03 18:52:35 -0700

    Add working timer and wordcount functions

--- src/StrategyCard/StrategyCard.js
+++ src/StrategyCard/StrategyCard.js
@@ -1,11 +1,11 @@
 import './StrategyCard.css';
 import React from 'react';
 
-function StrategyCard({strategy}) {
+function StrategyCard({strategy, timer}) {
     return (
        <div className='card'>
         <h2 className='strategy'>{strategy.strategy}</h2>
-        <p className='timer'>32</p>
+        <p className='timer'>{timer}</p>
        </div>
     );
   };
--- src/WritingArea/WritingArea.js
+++ src/WritingArea/WritingArea.js
@@ -7,14 +7,42 @@ function WritingArea() {
     const [currentStrat, setCurrentStrat] = useState({strategy: ''})
     const [writing, setWriting] = useState('')
     const [timer, setTimer] = useState(90)
+    const [timeoutID, setTimeoutID] = useState(0)
+    const [mostWords, setmostWords] = useState(0)
     
     useEffect(() => {
         returnStrategy()
         .then((data) => {
             setCurrentStrat(data)
         })
-        
       }, [])
+
+      useEffect(() => {
+        clock()
+      }, [timer])
+
+      useEffect(() => {
+        if ((writing.split(' ').length - 1) > mostWords) {
+            setmostWords(writing.split(' ').length - 1)
+            setTimer(90)
+        }
+      }, [writing])
+    
+    const clock = () =>{
+        clearTimeout(timeoutID)
+        if (timer > 0) {
+            setTimeoutID(setTimeout(() => {
+                setTimer(timer-1)
+            }, 1000))
+        }
+        if (timer === 0) {
+            returnStrategy()
+            .then((data) => {
+                setCurrentStrat(data)
+                setTimer(90)
+            })
+        }
+    }
 
     return (
         <>
@@ -26,7 +54,7 @@ function WritingArea() {
                 <StrategyCard strategy={currentStrat} timer={timer} />
             </article>
             <footer>
-            <div className='word-count'>{writing.split(' ').length}/750</div>
+            <div className='word-count'>{writing.split(' ').length - 1}/750</div>
             <a className='submit-button'>Submit</a>
             </footer>
         </>
