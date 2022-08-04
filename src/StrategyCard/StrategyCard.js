ca81d6d64f8b7503b45c0d5f366c1abf8dcc8b9c
David Daw <daviddaw@gmail.com>
2022-08-03 17:19:31 -0700

    Adds component files with initial layout

---
+++ src/StrategyCard/StrategyCard.js
@@ -0,0 +1,13 @@
+import './StrategyCard.css';
+import React from 'react';
+
+function StrategyCard() {
+    return (
+       <div className='card'>
+        <h2 className='strategy'>Use An Old Idea.</h2>
+        <p className='timer'>32</p>
+       </div>
+    );
+  };
+
+ export default StrategyCard
